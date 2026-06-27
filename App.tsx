aea39b7cbe640092f8bb560cb09ed9271d932451
Omar <al05-002-0323@utdelacosta.edu.mx>
2026-06-26 21:47:01 -0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import "./global.css";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -6,10 +5,11 @@ import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { AuthProvider } from "./src/store/AuthContext";
 import AppNavigator from "./src/navigation/AppNavigator";
+import { registerRootComponent } from "expo";
 
 const queryClient = new QueryClient();
 
-export default function App() {
+function App() {
   return (
     <SafeAreaProvider>
       <QueryClientProvider client={queryClient}>
@@ -23,3 +23,7 @@ export default function App() {
     </SafeAreaProvider>
   );
 }
+
+registerRootComponent(App);
+
+export default App;
