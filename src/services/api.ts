90b360771b77c439b4ab2f26868d39c290e1e7cd
Omar <al05-002-0323@utdelacosta.edu.mx>
2026-06-26 23:40:39 -0700

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
+import { Platform } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+const fallbackHost = Platform.OS === "android" ? "10.0.2.2" : "localhost";
+const baseURL = process.env.EXPO_PUBLIC_API_URL || `http://${fallbackHost}:8000/api`;
+
 const api = axios.create({
-  baseURL: process.env.EXPO_PUBLIC_API_URL || "http://localhost:8000/api",
+  baseURL,
   headers: {
     "Content-Type": "application/json",
   },
@@ -12,7 +16,10 @@ const api = axios.create({
 api.interceptors.request.use(async (config) => {
   const token = await AsyncStorage.getItem("access_token");
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    if (!config.headers) {
+      config.headers = {} as any;
+    }
+    (config.headers as any).Authorization = `Bearer ${token}`;
   }
   return config;
 });
