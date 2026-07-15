623145c82f1f804650c3e5c7634a737c0dd5ce22
adriansdevtic@gmail.com <adriansdevtic@gmail.com>
2026-07-14 19:45:26 -0700

    fix(lint): remove stale global declarations and unused eslint-disable directives

--- src/utils/apiError.ts
+++ src/utils/apiError.ts
@@ -1,4 +1,4 @@
-/* global console, __DEV__ -- console + RN dev-mode flag set by Metro bundler */
+/* global __DEV__ -- RN dev-mode flag set by Metro bundler */
 import type { AxiosError } from 'axios';
 import axios from 'axios';
 
