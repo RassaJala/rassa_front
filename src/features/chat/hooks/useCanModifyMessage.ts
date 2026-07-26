73887c637cef7020471764807ad9f609a270fa93
Gerardo Sanchez <111543422+gsanzz@users.noreply.github.com>
2026-07-25 17:33:33 -0700

    fix(chat): remove useMemo in useCanModifyMessage to clear eslint error

--- src/features/chat/hooks/useCanModifyMessage.ts
+++ src/features/chat/hooks/useCanModifyMessage.ts
@@ -1,10 +1,8 @@
-import { useMemo } from 'react';
-
 import { canModifyMessage } from '@rassa/chat';
 import type { CanModifyResult } from '@rassa/chat';
 
 import type { Message } from '@/types/chat';
 
 export function useCanModifyMessage(message: Message): CanModifyResult {
-  return useMemo(() => canModifyMessage(message), [message]);
+  return canModifyMessage(message);
 }
