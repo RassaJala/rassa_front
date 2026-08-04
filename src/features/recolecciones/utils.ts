55ac0e998c094bad11cfd1707ddced82c38ab064
Kevin lalito <kevinqu1nteroarre@gmail.com>
2026-08-04 09:54:54 -0600

    fix(lint): reordenar imports antes de exports

--- src/features/recolecciones/utils.ts
+++ src/features/recolecciones/utils.ts
@@ -1,7 +1,7 @@
 import { DIAS } from './constants';
+import { formatFechaHeader as formatFechaHeaderCommon } from '@/common/utils/recolecciones';
 export { buildDuplicateKeys, esRecoleccionDuplicada, ocupaFechaParaDuplicado, recoleccionDuplicateKey, validateProgramarForm, type ProgramarFormValues } from '@/common/utils/recolecciones';
 export { addDays, formatHora, isValidFecha, isValidFechaFormato, isValidHora, normalizeHora, parseFecha, todayString, toDateString } from '@/common/utils/recolecciones';
-import { formatFechaHeader as formatFechaHeaderCommon } from '@/common/utils/recolecciones';
 
 export function formatFechaHeader(fecha: string, today: string): string {
   return formatFechaHeaderCommon(fecha, today, DIAS);
--- web/src/utils/recolecciones.ts
+++ web/src/utils/recolecciones.ts
@@ -1,7 +1,7 @@
 import { DIAS } from '../constants/recolecciones';
+import { formatFechaHeader as formatFechaHeaderCommon } from '@/common/utils/recolecciones';
 export { buildDuplicateKeys, esRecoleccionDuplicada, ocupaFechaParaDuplicado, recoleccionDuplicateKey, validateProgramarForm, type ProgramarFormValues } from '@/common/utils/recolecciones';
 export { addDays, formatHora, isValidFecha, isValidFechaFormato, isValidHora, normalizeHora, parseFecha, todayString, toDateString } from '@/common/utils/recolecciones';
-import { formatFechaHeader as formatFechaHeaderCommon } from '@/common/utils/recolecciones';
 
 export function nombreCompletoAgricultor(a: {
   readonly nombre: string;
