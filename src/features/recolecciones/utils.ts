ede17d93e156f7ba98e2362eae87229181d26295
Kevin lalito <kevinqu1nteroarre@gmail.com>
2026-08-04 09:58:27 -0600

    fix(lint): internal imports before sibling imports

--- src/features/recolecciones/utils.ts
+++ src/features/recolecciones/utils.ts
@@ -1,5 +1,5 @@
-import { DIAS } from './constants';
 import { formatFechaHeader as formatFechaHeaderCommon } from '@/common/utils/recolecciones';
+import { DIAS } from './constants';
 export { buildDuplicateKeys, esRecoleccionDuplicada, ocupaFechaParaDuplicado, recoleccionDuplicateKey, validateProgramarForm, type ProgramarFormValues } from '@/common/utils/recolecciones';
 export { addDays, formatHora, isValidFecha, isValidFechaFormato, isValidHora, normalizeHora, parseFecha, todayString, toDateString } from '@/common/utils/recolecciones';
 
